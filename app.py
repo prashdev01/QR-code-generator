5995ea5cf06254b5bdaf026e3d9d683d3caefb6c
prashdev01 <prashantwadhave5@gmail.com>
2024-01-05 10:25:16 +0530

    the basic functionality and basic UI is created

---
+++ app.py
@@ -0,0 +1,25 @@
+from flask import Flask,render_template ,request ,send_file 
+import qrcode as qr
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/generate_qr', methods=['POST'])
+def generate_qr():
+    url = request.form.get('url')
+    if not url:
+        return "Please enter a URL"
+
+    # Genrate QR code 
+    
+    img = qr.make(url)
+    img.save("./static/qrcode.png")
+    # return send_file("static/qrcode.png" ,as_attachment=True),
+    return render_template('qr_gen.html')
+    
+    
+if __name__ == ('__main__'):
+    app.run(debug=True)
